f83cb6f8842c1658da094c5f5736f2fd536fe314
wambuivivian <miriivivianaa@gmail.com>
2026-07-23 11:45:23 +0300

    new staff

--- person.py
+++ person.py
@@ -6,3 +6,6 @@ class Person:
     def have_birthday(self):
         self.age +=1
         return f"Happy birthday {self.name}! You are now {self.age} years old."
+
+    def greet(self):
+        return f"Hello, my name is {self.name}and i am {self.age} years old"     
