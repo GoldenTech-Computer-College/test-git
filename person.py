4e3ac87f332fb5fb7bdbe88247ee26a5654b31b9
Ferdinand35 <kudaferdinand8@gmail.com>
2026-07-23 11:43:12 +0300

    birthday

--- person.py
+++ person.py
@@ -2,3 +2,7 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+    def have_birthday(self):
+        self.age +=1
+        return f"happy birthday! you are now {self.age} years old."
